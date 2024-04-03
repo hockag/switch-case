374d694a21c5e86744c4585465daf487c4e9ddc5
Rostyslav <124372762+hockag@users.noreply.github.com>
2024-04-03 21:11:57 +0300

    save

--- script.js
+++ script.js
@@ -64,3 +64,60 @@ function getSeason() {
     document.getElementById("result").innerHTML = season;
 }
 // 4
+function getDays() {
+    let month = document.getElementById("month-2").value;
+    let year = new Date().getFullYear(); // поточний рік
+    let days = new Date(year, month, 0).getDate();
+
+    document.getElementById("result-2").innerHTML = `У місяці ${month} - ${days} днів.`;
+}
+// 5
+
+function getColorAction() {
+    let color = document.querySelector('#color').value;
+    let action;
+
+    switch (color) {
+        case 'червоний':
+            action = "Стоп";
+            break;
+        case 'зелений':
+            action = 'йти';
+            break;
+        case 'жовтий':
+            action = 'чекати';
+            break;
+        default:
+            action = 'Колір не розпізнано.';
+    }
+    document.querySelector('#result-3').innerHTML = action;
+}
+// 6
+
+function calculate() {
+    let num1 = document.querySelector('#num1').value;
+    let num2 = document.querySelector('#num2').value;
+    let operation = document.querySelector('#operation').value;
+    let result4;
+
+    switch (operation) {
+        case '+':
+            result4 = Number(num1) + Number(num2);
+            break;
+        case '-':
+            result4 = Number(num1) - Number(num2);
+            break;
+        case '*':
+            result4 = Number(num1) * Number(num2);
+            break;
+        case '/':
+            if (num2 === 0 ) {
+                alert("Ділення на нуль неможливе");
+                return;
+            }
+            result4 = Number(num1) / Number(num2);
+            break;
+    }
+    document.getElementById("result-4").innerHTML = `Результат: ${result4}`;
+}
+
